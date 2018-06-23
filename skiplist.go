cf71148fcd9be532c96e18440f637d4eef31f359
mauricegit <maurice.tollmien@gmail.com>
2018-06-23 18:03:13 +0200

    adding the already working but unoptimized implementation

---
+++ skiplist.go
@@ -0,0 +1,371 @@
+// MIT License
+//
+// Copyright (c) 2018 Maurice Tollmien (maurice.tollmien@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// tree23 is an implementation for a balanced 2-3-tree.
+// It distinguishes itself from other implementations of 2-3-trees by having a few more
+// functions defined for finding elements close to a key (similar to possible insert positions in the tree)
+// for floating point keys and by having a native function to retreive the next or previous leaf SkipListElement
+// in the tree without knowing its key or position in the tree that work in O(1) for every leaf!
+// The last SkipListElement links to the first and the first back to the last SkipListElement.
+// The tree has its own memory manager to avoid frequent allocations for single nodes that are created or removed.
+package skiplist
+
+import (
+    "fmt"
+    "math/bits"
+    "math/rand"
+    "time"
+)
+
+
+type ListElement interface {
+    Compare(e ListElement) int
+    String() string
+}
+
+type SkipListPointer struct {
+    prev *SkipListElement
+    next *SkipListElement
+}
+
+type SkipListElement struct {
+    array       []SkipListPointer
+    level       int
+    value       ListElement
+}
+
+type SkipList struct {
+    levels              [25]*SkipListElement
+    maxNewLevel         int
+    maxLevel       int
+}
+
+// Package initialization
+func init() {
+    seed := time.Now().UTC().UnixNano()
+    seed = 1529743902965759434
+    fmt.Printf("seed: %v\n", seed)
+    rand.Seed(seed)
+}
+
+func generateLevel(maxLevel int) int {
+    // First we apply some mask which makes sure that we don't get a level
+    // above our desired level. Then we find the first set bit.
+    var x uint64 = rand.Uint64() & ((1 << uint(maxLevel-1)) -1)
+    zeroes := bits.TrailingZeros64(x)
+    if zeroes <= maxLevel {
+        return zeroes
+    }
+    return maxLevel-1
+}
+
+func New() SkipList {
+    return SkipList{[25]*SkipListElement{}, 25, 0}
+}
+
+func (t *SkipList) isEmpty() bool {
+    return t.levels[0] == nil
+}
+
+func (localRoot *SkipListElement) insertRec(e *SkipListElement, height, level int) {
+
+    // Next one is not overshot -- We go right!
+    next := localRoot.array[height].next
+    if next != nil && next.value.Compare(e.value) < 0 {
+        next.insertRec(e, height, level)
+        return
+    }
+
+    oldNext := localRoot.array[height].next
+
+    // Our level is now the same as height. So we have to squeeze our new SkipListElement in between.
+    if level >= height && (oldNext == nil || e.value.Compare(oldNext.value) < 0) {
+
+        if oldNext != nil {
+            oldNext.array[height].prev = e
+        }
+        e.array[height].next = oldNext
+        e.array[height].prev = localRoot
+        localRoot.array[height].next = e
+    }
+
+    if height > 0 {
+        localRoot.insertRec(e, height-1, level)
+    }
+}
+
+func (t *SkipList) Insert(e ListElement) {
+
+    level := generateLevel(t.maxNewLevel)
+    elem  := &SkipListElement{make([]SkipListPointer, level+1, level+1), level, e}
+
+
+    newFirst := true
+    if !t.isEmpty() {
+        newFirst = t.levels[0].value.Compare(e) > 0
+    }
+    entryIndex := t.maxLevel
+    // Find good entry point so we don't accidently skip half the list.
+    if !newFirst {
+        for i := t.maxLevel; i >= 0; i-- {
+            if t.levels[i] != nil && t.levels[i].value.Compare(e) < 0 {
+                entryIndex = i
+                break
+            }
+        }
+
+        if !t.isEmpty() {
+            t.levels[entryIndex].insertRec(elem, entryIndex, level)
+        }
+    }
+
+    if level > t.maxLevel {
+        t.maxLevel = level
+    }
+
+    // Where we have a left-most position that needs to be referenced!
+    for  i := level; i >= 0; i-- {
+        if newFirst || elem.array[i].prev == nil {
+            if t.levels[i] != nil {
+                t.levels[i].array[i].prev = elem
+            }
+            elem.array[i].next = t.levels[i]
+            t.levels[i] = elem
+
+        } else {
+            break
+        }
+    }
+}
+
+func (localRoot *SkipListElement) findRec(e ListElement, height int) (*SkipListElement, bool) {
+
+    next := localRoot.array[height].next
+    if next != nil && next.value.Compare(e) <= 0 {
+        return next.findRec(e, height)
+    }
+
+    if localRoot.value.Compare(e) == 0 {
+        return localRoot, true
+    }
+
+    if height > 0 {
+        return localRoot.findRec(e, height-1)
+    }
+
+    return nil, false
+
+}
+
+func (t *SkipList) Find2(e ListElement) (*SkipListElement, bool) {
+    if t.isEmpty() {
+        return nil, false
+    }
+
+    entryIndex := 0
+    // Find good entry point so we don't accidently skip half the list.
+    for i := t.maxLevel; i >= 0; i-- {
+        if t.levels[i] != nil && t.levels[i].value.Compare(e) <= 0 {
+            entryIndex = i
+            break
+        }
+    }
+
+    return t.levels[entryIndex].findRec(e, entryIndex)
+}
+
+func (t *SkipList) Find(e ListElement) (*SkipListElement, bool) {
+    if t.isEmpty() {
+        return nil, false
+    }
+
+    index := 0
+    // Find good entry point so we don't accidently skip half the list.
+    for i := t.maxLevel; i >= 0; i-- {
+        if t.levels[i] != nil && t.levels[i].value.Compare(e) <= 0 {
+            index = i
+            break
+        }
+    }
+    currentNode := t.levels[index]
+
+    currCompare := currentNode.value.Compare(e)
+    nextCompare := 0
+
+    for {
+        if currCompare == 0 {
+            return currentNode, true
+        }
+
+        nextNode := currentNode.array[index].next
+        if nextNode != nil {
+            nextCompare = nextNode.value.Compare(e)
+            currCompare = nextCompare
+        }
+
+        if nextNode != nil && nextCompare <= 0 {
+            currentNode = nextNode
+        } else {
+            if index > 0 {
+                index--
+            } else {
+                return nil, false
+            }
+        }
+    }
+
+    return nil, false
+
+}
+
+func (t *SkipList) Delete(e ListElement) {
+
+    if elem,ok := t.Find(e); ok {
+        for i := elem.level; i >= 0; i-- {
+            prev := elem.array[i].prev
+            next := elem.array[i].next
+
+            if prev != nil {
+                prev.array[i].next = next
+            }
+            if next != nil {
+                next.array[i].prev = prev
+            }
+
+            if e.Compare(t.levels[i].value) == 0 {
+                t.levels[i] = next
+                if next == nil {
+                    // reduce the maximum entry position!
+                    t.maxLevel = i-1
+                }
+            }
+        }
+    }
+}
+
+func (localRoot *SkipListElement) deleteRec(e ListElement, height int) *SkipListElement {
+
+    next := localRoot.array[height].next
+    if next != nil && (next.value.Compare(e) <= 0) {
+        return next.deleteRec(e, height)
+    }
+
+    isEqual := localRoot.value.Compare(e) == 0
+    // Our level is now the same as height and we found our SkipListElement!
+    if isEqual {
+        next := localRoot.array[height].next
+        prev := localRoot.array[height].prev
+
+        if next != nil {
+            next.array[height].prev = prev
+        }
+        if prev != nil {
+            prev.array[height].next = next
+        }
+    }
+
+    if height > 0 {
+        return localRoot.deleteRec(e, height-1)
+    }
+
+    if isEqual {
+        return localRoot
+    }
+    return nil
+}
+
+func (t *SkipList) Delete2(e ListElement) {
+    if t.isEmpty() {
+        return
+    }
+
+    entryIndex := 0
+    // Find good entry point so we don't accidently skip half the list.
+    for i := t.maxLevel; i >= 0; i-- {
+        if t.levels[i] != nil && t.levels[i].value.Compare(e) <= 0 {
+            entryIndex = i
+            break
+        }
+    }
+
+    removedNode := t.levels[entryIndex].deleteRec(e, entryIndex)
+
+
+
+    if removedNode != nil {
+        // Re-set entry pointers because we just removed the first one!
+        for i := entryIndex; i >= 0; i-- {
+            if e.Compare(t.levels[i].value) == 0 {
+                next := removedNode.array[i].next
+                t.levels[i] = next
+                if next == nil {
+                    // reduce the maximum entry position!
+                    t.maxLevel = i-1
+                }
+            } else {
+                break
+            }
+        }
+    }
+
+}
+
+func (t *SkipList) PrettyPrint() {
+
+    fmt.Printf("--> ")
+    for i,l := range t.levels {
+        next := "---"
+        if l != nil {
+            next = l.value.String()
+        }
+        fmt.Printf("[---|%v]", next)
+        if i < len(t.levels)-1 {
+            fmt.Printf(" --> ")
+        }
+    }
+    fmt.Println("")
+
+    node := t.levels[0]
+    for node != nil {
+        fmt.Printf("%v: ", node.value)
+        for i,l := range node.array {
+
+            prev := "---"
+            if l.prev != nil {
+                prev = l.prev.value.String()
+            }
+            next := "---"
+            if l.next != nil {
+                next = l.next.value.String()
+            }
+
+            fmt.Printf("[%v|%v]", prev, next)
+            if i < len(node.array)-1 {
+                fmt.Printf(" --> ")
+            }
+
+        }
+        fmt.Printf("\n")
+        node = node.array[0].next
+    }
+    fmt.Printf("\n")
+}
---
+++ skiplist_test.go
@@ -0,0 +1,175 @@
+package skiplist
+
+import (
+    "fmt"
+    "time"
+    "testing"
+    "math/rand"
+    //"strconv"
+    "github.com/pkg/profile"
+)
+
+
+var g_maxN int = 1000000
+
+type Element struct {
+    E int
+}
+
+func (e Element) Compare(e2 ListElement) int {
+
+    if e.E < e2.(Element).E {
+        return -1
+    }
+    if e.E == e2.(Element).E {
+        return 0
+    }
+    return 1
+}
+func (e Element) String() string {
+    //return strconv.Itoa(e.E)
+    return fmt.Sprintf("%03d", e.E)
+}
+
+
+// timeTrack will print out the number of nanoseconds since the start time divided by n
+// Useful for printing out how long each iteration took in a benchmark
+func timeTrack(start time.Time, n int, name string) {
+    loopNS := time.Since(start).Nanoseconds() / int64(n)
+    fmt.Printf("%s: %d\n", name, loopNS)
+}
+
+
+func TestBenchmarkInsert(t *testing.T) {
+    list := New()
+
+    defer timeTrack(time.Now(), g_maxN, "mtInserts")
+
+    for i := 0; i < g_maxN; i++ {
+        list.Insert(Element{g_maxN-i})
+    }
+}
+func TestBenchmarkWorstInsert(t *testing.T) {
+    list := New()
+
+    defer timeTrack(time.Now(), g_maxN, "mtWorstInserts")
+
+    for i := 0; i < g_maxN; i++ {
+        list.Insert(Element{i})
+    }
+}
+func TestBenchmarkAvgSearch(t *testing.T) {
+    list := New()
+
+    for i := 0; i < g_maxN; i++ {
+        list.Insert(Element{i})
+    }
+
+    defer timeTrack(time.Now(), g_maxN, "mtAvgSearch")
+
+    for i := 0; i < g_maxN; i++ {
+        list.Find(Element{i})
+    }
+}
+func TestBenchmarkSearchEnd(t *testing.T) {
+    list := New()
+
+    for i := 0; i < g_maxN; i++ {
+        //fmt.Printf("Insert: %d\n", i)
+        list.Insert(Element{i})
+
+
+
+    }
+
+    defer timeTrack(time.Now(), g_maxN, "mtSearchEnd")
+
+    for i := 0; i < g_maxN; i++ {
+        list.Find(Element{g_maxN-1})
+    }
+}
+func TestBenchmarkDelete(t *testing.T) {
+    list := New()
+
+    for i := 0; i < g_maxN; i++ {
+        list.Insert(Element{i})
+    }
+
+    defer timeTrack(time.Now(), g_maxN, "mtDelete")
+
+    for i := 0; i < g_maxN; i++ {
+        list.Delete(Element{i})
+    }
+}
+func TestBenchmarkWorstDelete(t *testing.T) {
+    list := New()
+
+    for i := 0; i < g_maxN; i++ {
+        list.Insert(Element{i})
+    }
+
+    defer timeTrack(time.Now(), g_maxN, "mtWorstDelete")
+
+    for i := 0; i < g_maxN; i++ {
+        list.Delete(Element{g_maxN-i})
+    }
+}
+
+func TestFind(t *testing.T) {
+    list := New()
+
+    for i := 0; i < g_maxN; i++ {
+        list.Insert(Element{i})
+    }
+    for i := 0; i < g_maxN; i++ {
+
+        if e,ok := list.Find(Element{i}); !ok || e == nil {
+            t.Fail()
+        }
+    }
+}
+
+func TestDelete(t *testing.T) {
+
+
+    list := New()
+
+    for i := 0; i < g_maxN; i++ {
+        list.Insert(Element{i})
+    }
+
+    //list.PrettyPrint()
+
+    for i := 0; i < g_maxN; i++ {
+        list.Delete(Element{i})
+    }
+
+    if !list.isEmpty() {
+        t.Fail()
+    }
+}
+
+func TestInsertRandom(t *testing.T) {
+
+    defer profile.Start(profile.CPUProfile).Stop()
+    list := New()
+
+    rList := rand.Perm(g_maxN)
+    for _,e := range rList {
+        list.Insert(Element{e})
+    }
+
+    for _,e := range rList {
+        if e2,ok := list.Find(Element{e}); !ok || e2 == nil {
+            t.Fail()
+        }
+    }
+
+    for _,e := range rList {
+        list.Delete(Element{e})
+    }
+
+    if !list.isEmpty() {
+        t.Fail()
+    }
+}
